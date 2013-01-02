0d3c50329474ebbc3acc20c58a4b4ed978bfd45e
jakaldama <jakaldama@gmail.com>
2013-01-02 13:01:10 -0500

    first commit

---
+++ Stack/src/Node.java
@@ -0,0 +1,25 @@
+
+public class Node {
+	int num;
+	Node next;
+	
+	Node() {
+		
+	}
+	
+	public int getNum() {
+		return num;
+	}
+	
+	public void setNum(int n) {
+		this.num = n;
+	}
+	
+	public Node getNext() {
+		return next;
+	}
+	
+	public void setNext(Node next) {
+		this.next = next;
+	}
+}
---
+++ Stack/src/Stack.java
@@ -0,0 +1,31 @@
+
+public class Stack {
+	private static int size;
+	private static Node head;
+	
+	public Stack() {
+		head = null;
+		size = 0;
+	}
+	
+	public void getSize() {
+		System.out.println(size);
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	public void push(int n) {
+		Node z = new Node();
+		z.setNum(n);
+		z.setNext(head);
+		head = z;
+		size++;
+	}
+	
+	public void pop() {
+		if (!isEmpty()) head = head.getNext();
+		size--;
+	}
+}
